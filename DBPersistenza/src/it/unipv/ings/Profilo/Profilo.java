afcbd5f1903318eb8d54cae363b449b224408894
davide <davide@192.168.56.1>
2022-02-11 13:29:58 +0100

    Gruppo, messaggio di gruppo, messaggio privato e profilo per database

---
+++ DBPersistenza/src/it/unipv/ings/Profilo/Profilo.java
@@ -0,0 +1,104 @@
+package it.unipv.ings.Profilo;
+
+public class Profilo {
+public Profilo(String idProfilo, String nickname, String descrizione, int numFollower, int numSeguiti, int numPost,
+			String tipo, String messaggioDiGruppo, String messaggioPrivato, String storia, String utente) {
+		super();
+		this.idProfilo = idProfilo;
+		this.nickname = nickname;
+		this.descrizione = descrizione;
+		this.numFollower = numFollower;
+		this.numSeguiti = numSeguiti;
+		this.numPost = numPost;
+		this.tipo = tipo;
+		this.messaggioDiGruppo = messaggioDiGruppo;
+		this.messaggioPrivato = messaggioPrivato;
+		this.storia = storia;
+		this.utente = utente;
+	}
+private String idProfilo;
+private String nickname;
+private String descrizione;
+private int numFollower;
+private int numSeguiti;
+private int numPost;
+private String tipo;
+private String messaggioDiGruppo;
+private String messaggioPrivato;
+private String storia;
+private String utente;
+public String getIdProfilo() {
+	return idProfilo;
+}
+public void setIdProfilo(String idProfilo) {
+	this.idProfilo = idProfilo;
+}
+public String getNickname() {
+	return nickname;
+}
+public void setNickname(String nickname) {
+	this.nickname = nickname;
+}
+public String getDescrizione() {
+	return descrizione;
+}
+public void setDescrizione(String descrizione) {
+	this.descrizione = descrizione;
+}
+public int getNumFollower() {
+	return numFollower;
+}
+public void setNumFollower(int numFollower) {
+	this.numFollower = numFollower;
+}
+public int getNumSeguiti() {
+	return numSeguiti;
+}
+public void setNumSeguiti(int numSeguiti) {
+	this.numSeguiti = numSeguiti;
+}
+public int getNumPost() {
+	return numPost;
+}
+public void setNumPost(int numPost) {
+	this.numPost = numPost;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+public String getMessaggioDiGruppo() {
+	return messaggioDiGruppo;
+}
+public void setMessaggioDiGruppo(String messaggioDiGruppo) {
+	this.messaggioDiGruppo = messaggioDiGruppo;
+}
+public String getMessaggioPrivato() {
+	return messaggioPrivato;
+}
+public void setMessaggioPrivato(String messaggioPrivato) {
+	this.messaggioPrivato = messaggioPrivato;
+}
+public String getStoria() {
+	return storia;
+}
+public void setStoria(String storia) {
+	this.storia = storia;
+}
+public String getUtente() {
+	return utente;
+}
+public void setUtente(String utente) {
+	this.utente = utente;
+}
+@Override
+public String toString() {
+	return "Profilo [idProfilo=" + idProfilo + ", nickname=" + nickname + ", descrizione=" + descrizione
+			+ ", numFollower=" + numFollower + ", numSeguiti=" + numSeguiti + ", numPost=" + numPost + ", tipo=" + tipo
+			+ ", messaggioDiGruppo=" + messaggioDiGruppo + ", messaggioPrivato=" + messaggioPrivato + ", storia="
+			+ storia + ", utente=" + utente + "]";
+}
+
+}
